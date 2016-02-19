b7a1632dbf9ecc7424b93fbb5b78dc122e3ae972
ajay mahicha <ajaymahicha@gmail.com>
2016-02-18 23:58:34 -0800

    first page

--- app/src/main/java/com/agents/cop290/MainActivity.java
+++ app/src/main/java/com/agents/cop290/MainActivity.java
@@ -1,7 +1,11 @@
 package com.agents.cop290;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView a = (TextView) findViewById(R.id.quote);
+        Random rand = new Random();
+        int e = rand.nextInt(7);
+        String[] q = getResources().getStringArray(R.array.quotes);
+        a.setText(q[e]);
+       int i=0;
+        while(i<5000)
+        {
+            i++;
+        }
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+
     }
+
+
 }
