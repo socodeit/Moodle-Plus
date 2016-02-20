8d9c2a7c544bd6d27d4e2e8ef169df7baa18eddb
ritikvijay <coolricks96@gmail.com>
2016-02-20 07:02:31 +0530

    Complete Listview without Cookies

--- app/src/main/java/com/agents/cop290/CourseListStudents.java
+++ app/src/main/java/com/agents/cop290/CourseListStudents.java
@@ -109,26 +109,27 @@ public class CourseListStudents extends AppCompatActivity {
 //        }
 
     String[] array_courses;
-    String Cookie="";
+    ListView listView;
+    //String Cookie="";
 
 
 
-    private static CourseListStudents _instance;
-    private RequestQueue _requestQueue;
-    private SharedPreferences _preferences;
-
-    public static CourseListStudents get() {
-        return _instance;
-    }
+//    private static CourseListStudents _instance;
+//    private RequestQueue _requestQueue;
+//    private SharedPreferences _preferences;
+//
+//    public static CourseListStudents get() {
+//        return _instance;
+//    }
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_list_students);
 
-        _instance = this;
-        _preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        _requestQueue = Volley.newRequestQueue(this);
+//        _instance = this;
+//        _preferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        _requestQueue = Volley.newRequestQueue(this);
 
         Intent i = getIntent();
         Bundle extras = i.getExtras();
@@ -162,7 +163,7 @@ public class CourseListStudents extends AppCompatActivity {
                     //adding Position, first name and last name to welcome message
                     final TextView welcomeMsg = (TextView) findViewById(R.id.welcomeMsg);
                     welcomeMsg.setText("Welcome" + " " + position + " " + first_name + " " + last_name);
-                    Toast.makeText(getApplicationContext(),Cookie,Toast.LENGTH_LONG).show();
+                    //Toast.makeText(getApplicationContext(),Cookie,Toast.LENGTH_LONG).show();
 
 
                 } catch (JSONException e) {
@@ -176,15 +177,16 @@ public class CourseListStudents extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_LONG).show();
 
             }
-        }){
-            @Override
-            protected Response parseNetworkResponse(NetworkResponse response) {
-                Map headers = response.headers;
-                Cookie = (String) headers.get("Set-Cookie");
-                return super.parseNetworkResponse(response);
-            }
-        };
-        _requestQueue.add(myReq);
+        });
+//        {
+//            @Override
+//            protected Response parseNetworkResponse(NetworkResponse response) {
+//                Map headers = response.headers;
+//                Cookie = (String) headers.get("Set-Cookie");
+//                return super.parseNetworkResponse(response);
+//            }
+//        };
+//        _requestQueue.add(myReq);
 
         //receiving info about courses
         String url2 = mainUrl+"courses/list.json";
@@ -193,7 +195,7 @@ public class CourseListStudents extends AppCompatActivity {
             public void onResponse(JSONObject course_response) {
                 try {
                     //accessing course_info, current sem
-                    //Double current_sem = course_response.getDouble("current_sem");
+                    Double current_sem = course_response.getDouble("current_sem");
                     JSONArray course_list = course_response.getJSONArray("courses");
 
                     //adding all course names to listview's string array
@@ -205,7 +207,7 @@ public class CourseListStudents extends AppCompatActivity {
                         String course_name = course_object.optString("code").toString() + " : " + course_object.optString("name").toString();
                         array_courses[i] = course_name;
                     }
-                    Toast.makeText(getApplicationContext(), "Nothing to show",Toast.LENGTH_LONG).show();
+                   // Toast.makeText(getApplicationContext(), "Nothing to show",Toast.LENGTH_LONG).show();
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -218,29 +220,39 @@ public class CourseListStudents extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_LONG).show();
 
             }
-        }){
+        });
+//        {
+//            @Override
+//            public Map getHeaders() throws AuthFailureError {
+//                Map headers = new HashMap();
+//                if(!Cookie.equals(""))
+//                    headers.put("Cookie", Cookie);
+//                return headers;
+//            }
+//        };
+//        _requestQueue.add(myReq2);
+
+
+        //creating the listview of list of courses
+
+        listView = (ListView) findViewById(R.id.course_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_course_list_students, array_courses);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public Map getHeaders() throws AuthFailureError {
-                Map headers = new HashMap();
-                if(!Cookie.equals(""))
-                    headers.put("Cookie", Cookie);
-                return headers;
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String item = parent.getItemAtPosition(position).toString();
+                String[] abcd = item.split(":");
+                String courseCODE = abcd[0];
+                Intent i = new Intent(CourseListStudents.this, courseDetail.class);
+                i.putExtra("COURSECODE", courseCODE);
+                startActivity(i);
             }
-        };
-        _requestQueue.add(myReq2);
+        });
+
     }
 
-    //creating the listview of list of courses
-//            ListView listView;
-//            listView = (ListView) findViewById(R.id.course_list);
-//            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_course_list_students, array_courses);
-//            listView.setAdapter(adapter);
-//            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//                    @Override
-//                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//
-//                               }
-//                });
+
 //
 
 
